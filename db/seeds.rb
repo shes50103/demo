0c4164cbb5284f6ab134b4d1b8586385090adf49
shes50103 <shes50103@gmail.com>
2021-03-20 19:15:53 +0800

    Update seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,6 +1,8 @@
-10.times {
-  Post.create(
-    title: Faker::Name.unique.name,
-    body: Faker::Lorem.paragraphs
-  )
-}
+if Post.count == 0
+  10.times {
+    Post.create(
+      title: Faker::Name.unique.name,
+      body: Faker::Lorem.paragraphs
+    )
+  }
+end
